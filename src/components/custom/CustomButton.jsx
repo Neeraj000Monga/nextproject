5e7f5b19bbb48917de59c3cbbae84a098ad4a55e
Neeraj000Monga <neerajmonga000@gmail.com>
2024-08-06 20:39:06 +0530

    add custombutton

---
+++ src/components/custom/CustomButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CustomButton = () => {
+  return (
+    <button
+      className="font-bold text-black px-7 bg-[#AB87FF] text-[4rem]"
+      style={{ borderRadius: "60px", lineHeight: 1.2 }}
+    >
+      DEPLOY
+    </button>
+  );
+};
+
+export default CustomButton;
